41e408ec636c60e06110fc246aae4725b825a92b
kevin.poteau <kevin.poteau@e-mothep.com>
2018-07-09 15:13:36 +0200

    Aowua

--- src/com/projet/main/myMainClass.java
+++ src/com/projet/main/myMainClass.java
@@ -4,7 +4,7 @@ package com.projet.main;
 public class myMainClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.console().writer().println("Hello World");
 		
 		
 	}
